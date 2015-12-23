c91bcfbe8ce71d50b0aaae179a7e8a8f8d64c6f1
Rahul P <rahulpatil_scs@yahoo.co.in>
2015-12-24 01:45:33 +0530

    Added asset precompile configuration

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module YomuLab
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.serve_static_assets = true
     config.active_record.raise_in_transactional_callbacks = true
+
+   config.assets.initialize_on_precompile = false
+
   end
 end
