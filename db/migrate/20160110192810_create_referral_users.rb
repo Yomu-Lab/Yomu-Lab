3acc94a68f8ae1289c5188330cfe9a3545c139ca
Rahul P <rahulpatil_scs@yahoo.co.in>
2016-01-18 00:55:10 +0530

    Change in migration for boolean

--- db/migrate/20160110192810_create_referral_users.rb
+++ db/migrate/20160110192810_create_referral_users.rb
@@ -3,7 +3,7 @@ class CreateReferralUsers < ActiveRecord::Migration
     create_table :referral_users do |t|
       t.integer :user_id
       t.integer :referral_user_id
-      t.boolean :status, default: true
+      t.boolean :status, default: "true"
 
       t.timestamps null: false
     end
