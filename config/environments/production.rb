85bb6f75c2e28cb84bef46236cda3d2b52da923b
Rahul P <rahulpatil_scs@yahoo.co.in>
2016-01-22 03:27:21 +0530

    Change in production email-debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,15 +83,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
+    address: "imap.gmail.com",
+    port: 993,
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: 'support@yomulab.com',
     password: 'yomu@123'
   }  
-  
+
   config.action_mailer.raise_delivery_errors = true
 
 end
