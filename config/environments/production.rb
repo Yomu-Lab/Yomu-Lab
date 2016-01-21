0046132a0f44b43a7af47c72dae95339e36f666f
Rahul P <rahulpatil_scs@yahoo.co.in>
2016-01-22 03:04:29 +0530

    Change in production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,9 +88,10 @@ Rails.application.configure do
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: 'yomulab@gmail.com',
+    user_name: 'support@yomulab.com',
     password: 'yomu@123'
   }  
+  
   config.action_mailer.raise_delivery_errors = true
 
 end
